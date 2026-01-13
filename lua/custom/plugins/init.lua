b0f6005eaf935301ad8b89905fa4245ba85a4c47
frippuz <vilppu.tupeli@gmail.com>
2026-01-13 16:31:37 +0200

    Added copilot plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'CopilotC-Nvim/CopilotChat.nvim',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', branch = 'master' },
+  },
+  build = 'make tiktoken',
+  opts = {},
+  model = 'gpt-4.1', -- AI model to use
+  temperature = 0.1, -- Lower = focused, higher = creative
+  window = {
+    layout = 'vertical', -- 'vertical', 'horizontal', 'float'
+    width = 0.5, -- 50% of screen width
+  },
+  auto_insert_mode = true, -- Enter insert mode when opening
+}
