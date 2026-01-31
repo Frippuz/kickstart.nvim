a344cd5b3d6644cc684963203da6b54c5fc3c7f6
frippuz <vilppu.tupeli@gmail.com>
2026-01-31 22:55:21 +0200

    Add Vimtex

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,17 +3,30 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'CopilotC-Nvim/CopilotChat.nvim',
-  dependencies = {
-    { 'nvim-lua/plenary.nvim', branch = 'master' },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {},
+    model = 'gpt-4.1', -- AI model to use
+    temperature = 0.1, -- Lower = focused, higher = creative
+    window = {
+      layout = 'vertical', -- 'vertical', 'horizontal', 'float'
+      width = 0.5, -- 50% of screen width
+    },
   },
-  build = 'make tiktoken',
-  opts = {},
-  model = 'gpt-4.1', -- AI model to use
-  temperature = 0.1, -- Lower = focused, higher = creative
-  window = {
-    layout = 'vertical', -- 'vertical', 'horizontal', 'float'
-    width = 0.5, -- 50% of screen width
+
+-- Vimtex for LaTeX support
+  {
+    auto_insert_mode = true, -- Enter insert mode when opening
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
   },
-  auto_insert_mode = true, -- Enter insert mode when opening
 }
