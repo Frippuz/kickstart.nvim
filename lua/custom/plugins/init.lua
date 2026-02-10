c4a16738bfc71fe1017866a96253204123e9a9e7
tupeli <vilppu.tupeli@nokia.com>
2026-02-10 17:43:49 +0200

    removed nvim.surround

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -30,15 +30,4 @@ return {
       vim.g.vimtex_view_method = 'zathura'
     end,
   },
-
-  {
-    'kylechui/nvim-surround',
-    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
-  },
 }
