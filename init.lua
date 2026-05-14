7b4a61887be63815ea656300293a312c23cf8783
tupeli <vilppu.tupeli@nokia.com>
2026-05-14 14:45:54 +0300

    New keybinds

--- init.lua
+++ init.lua
@@ -298,11 +298,12 @@ vim.keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
 vim.keymap.set('v', '<A-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move Down' })
 vim.keymap.set('v', '<A-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move Up' })
 
--- NOTE: added from https://github.com/ThePrimeagen/init.lua 
+-- Added from https://github.com/ThePrimeagen/init.lua 
 -- More useful binds at https://github.com/adibhanna/nvim/blob/a2c16381/lua/config/keymaps.lua#L4-L48
 
 -- Paste over currently selected text without yanking it
 vim.keymap.set('x', '<leader>p', [["_dP]])
+
 -- Keep cursor centered when scrolling and searching
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
@@ -322,10 +323,6 @@ vim.keymap.set("n", "<CR>", "ciw", { noremap = true, silent = true })
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
--- better increment
-vim.keymap.set("v","<C-a>","<C-a>gv")
-vim.keymap.set("v","<C-x>","<C-x>gv")
-
 -- Move line on the screen rather than by line in the file
 vim.keymap.set("n", "j", "gj", { noremap = true, silent = true })
 vim.keymap.set("n", "k", "gk", { noremap = true, silent = true })
@@ -337,6 +334,17 @@ vim.keymap.set({ "n", "x", "o" }, "L", "g_", { noremap = true, silent = true })
 -- Own stuff 
 -- Max fold is one level lower
 vim.keymap.set('n', 'zM', 'zMzr')
+
+-- better increment
+vim.keymap.set("v","<C-a>","<C-a>gv")
+vim.keymap.set("v","<C-x>","<C-x>gv")
+
+-- Close buffer
+vim.keymap.set('n', '<C-b>d',':bd<CR>')
+
+-- Open terminal
+vim.keymap.set('n', '<C-t>s', '<C-w>s:ter<CR>')
+vim.keymap.set('n', '<C-t>v', '<C-w>v:ter<CR>')
 
 -- Format typst files starting from first header
 vim.keymap.set('n', 'gq', function()
